3e61e19564695cecbc839a4645df907b3549bf89
Vsevolod Stepanov <tehnar5@gmail.com>
2018-10-19 18:18:04 +0300

    Implement bitonic sort

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,4 +1,59 @@
-__kernel void bitonic(__global float* as)
-{
-    // TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#define WORK_GROUP_SIZE 256
+#endif
+
+#line 7
+
+#define READ_TO_LOCAL_MEM() ;
+
+__kernel void bitonic_small_array(__global float* as, unsigned int cur_size, unsigned int size, unsigned int n) {
+    unsigned int local_id = get_local_id(0);
+    unsigned int global_id = get_global_id(0);
+
+    __local float mem[WORK_GROUP_SIZE];
+
+    if (global_id < n) {
+        mem[local_id] = as[global_id];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    int is_sort_ascending = global_id % (2 * size) < size;
+
+    while (cur_size >= 1) {
+
+        if (global_id % (2 * cur_size) < cur_size && global_id + cur_size < n) {
+            float a = mem[local_id];
+            float b = mem[local_id + cur_size];
+            if ((a < b) == is_sort_ascending) {
+            } else {
+                mem[local_id] = b;
+                mem[local_id + cur_size] = a;
+            }
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+        cur_size /= 2;
+    }
+
+    if (global_id < n) {
+        as[global_id] = mem[local_id];
+    }
+}
+
+
+__kernel void bitonic_large_array(__global float* as, unsigned int cur_size, unsigned int size, unsigned int n) {
+    unsigned int global_id = get_global_id(0);
+
+    int is_sort_ascending = global_id % (2 * size) < size;
+
+    if (global_id % (2 * cur_size) < cur_size && global_id + cur_size < n) {
+        float a = as[global_id];
+        float b = as[global_id + cur_size];
+        if ((a < b) == is_sort_ascending) {
+        } else {
+            as[global_id] = b;
+            as[global_id + cur_size] = a;
+        }
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -23,6 +23,33 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+// Only arrays with length of power of 2 are supported
+void bitonic_sort(
+        ocl::Kernel &small_array_kernel,
+        ocl::Kernel &large_array_kernel,
+        const gpu::gpu_mem_32f &mem,
+        unsigned int n,
+        unsigned int work_group_size) {
+
+    unsigned int global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+
+    unsigned int size = 2;
+
+    unsigned int max_size = 2;
+    while (max_size < n) max_size *= 2;
+
+    while (size <= max_size) {
+        unsigned int cur_size = size / 2;
+
+        while (2 * cur_size > work_group_size) {
+            large_array_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), mem, cur_size, size, n);
+            cur_size /= 2;
+        }
+
+        small_array_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), mem, cur_size, size, n);
+        size *= 2;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -33,7 +60,7 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 50*1000*1000;
+    unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -52,13 +79,21 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
     {
-        ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
-        bitonic.compile();
+        unsigned int work_group_size = 256;
+        ocl::Kernel bitonic_small_array_kernel(
+                bitonic_kernel, bitonic_kernel_length, "bitonic_small_array",
+                "-DWORK_GROUP_SIZE=" + to_string(work_group_size));
+        ocl::Kernel bitonic_large_array_kernel(
+                bitonic_kernel, bitonic_kernel_length, "bitonic_large_array",
+                "-DWORK_GROUP_SIZE=" + to_string(work_group_size));
+
+        bitonic_small_array_kernel.compile();
+        bitonic_large_array_kernel.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -66,10 +101,7 @@ int main(int argc, char **argv)
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                         as_gpu, n);
+            bitonic_sort(bitonic_small_array_kernel, bitonic_large_array_kernel, as_gpu, n, work_group_size);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -78,10 +110,16 @@ int main(int argc, char **argv)
         as_gpu.readN(as.data(), n);
     }
 
+    for (int i = 0; i < n - 1; ++i) {
+        if (as[i] > as[i + 1]) {
+            printf("%d %f %f\n", i, as[i], as[i + 1]);
+        }
+//        printf("%f ", as[i]);
+    }
+
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
     return 0;
 }
